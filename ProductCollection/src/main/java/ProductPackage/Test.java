d90c984b7665e4ec14653752e208ebaf6a92ba52
ZirunWang11 <zirun.wang1102@gmail.com>
2021-07-15 11:20:42 -0400

    Third Commit

--- ProductCollection/src/main/java/ProductPackage/Test.java
+++ ProductCollection/src/main/java/ProductPackage/Test.java
@@ -2,21 +2,49 @@ package ProductPackage;
 
 import java.util.ArrayList;
 
+import com.google.gson.Gson;
+
 public class Test {
 	public static void Main(String[] arg) {
 		
 		Api testApi = new Api();
-		ArrayList<Product> productList = testApi.getProducts();
+		Gson gson = new Gson();
 		
-		if (productList.size() == 0) {
+		// test Read/GET function;
+		ArrayList<Product> productList = testApi.getProducts();
+		if (productList.size() == 0) { 
 			System.out.println("No product exit!!!");
 		} else {
-			//Creating the ObjectMapper object
-		      ObjectMapper mapper = new ObjectMapper();
-		      //Converting the Object to JSONString
-		      String jsonString = mapper.writeValueAsString(std);
-		      System.out.println(jsonString);
+			
+			System.out.print(gson.toJson(productList));
 		}
+		
+		// test Create/POST function;
+		Image flower = new Image("xxx", "a red flower");
+		Image sun = new Image("sxxxun", "rising sun");
+
+		ArrayList<Image> images = new ArrayList<>();
+		images.add(flower);
+		images.add(sun);
+
+		Album landscape = new Album(images, "the landscape Album");
+
+		Product p = new Product("product2", "my second landsacpe product", landscape);
+		
+		Product testProduct = testApi.createProduct(p);
+		
+		System.out.println(gson.toJson(testProduct));
+		
+		// test update/PUT function;
+		Product p3 = new Product("product3", "my fakeNew landsacpe product", landscape);
+		
+		Product testUpdateProduct = testApi.updateProduct("product2", p3);
+		System.out.println(gson.toJson(testUpdateProduct));
+		
+		// test delete/DELETE function;
+		System.out.println(testApi.deleteProduct("product1"));
+		
+		
 	}
 
 }
